d33ac32d42f40c2e78acc4acd35395f96b7ba7b7
ABRUIZ25 <89878760+ABRUIZ25@users.noreply.github.com>
2021-09-22 10:30:59 -0400

    planet

---
+++ space-boxing.js
@@ -0,0 +1,34 @@
+const prompt = require('prompt-sync')();
+
+const planet = number(prompt('please enter a planet'));
+
+if (planet ===1){
+const result = weight * 0.78
+console.log(result);
+}
+
+if (planet ===1){
+    const result = weight * 0.39
+    console.log(result);
+    }
+
+    if (planet ===1){
+        const result = weight * 2.65
+        console.log(result);
+        }
+
+        if (planet ===1){
+            const result = weight * 1.17
+            console.log(result);
+            }
+
+            if (planet ===1){
+                const result = weight * 1.05
+                console.log(result);
+                }
+
+                if (planet ===1){
+                    const result = weight * 1.23
+                    console.log(result);
+                    }
+                    console.log(result);
